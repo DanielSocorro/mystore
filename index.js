facf2398d67053daa006c905c6bcdcecc951d351
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-09-26 12:26:31 -0300

    params in paths and endpoints

--- index.js
+++ index.js
@@ -6,6 +6,40 @@ app.get('/', (req, res) => {
   res.send('hello, my server in express');
 })
 
+app.get('/new-path', (req, res) => {
+  res.send('hello, Im a new endpoint');
+})
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'product 1',
+      price: 1000
+    },
+    {
+      name: 'product 2',
+      price: 2000
+    }
+  ]);
+})
+
+app.get('/products/:id', (req, res) =>  {
+  const { id } = req.params;
+  res.json({
+      id,
+      name: 'product 2',
+      price: 2000
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res)=> {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
+})
+
 app.listen(port, () => {
   console.log('my port ' + port);
 });
